2eec070984484c97eb884d6a96242d0e2ae1d4fd
JC <g1447236976@gmail.com>
2025-08-01 19:30:52 +0800

    add clear history btn

--- medai-ios-objc/VC/MKHistoryViewController.m
+++ medai-ios-objc/VC/MKHistoryViewController.m
@@ -27,6 +27,30 @@
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"HistoryCell"];
     
 //    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
+                                              initWithTitle:@"Clear"
+                                              style:UIBarButtonItemStylePlain
+                                              target:self
+                                              action:@selector(clearHistoryTapped)];
+}
+
+- (void)clearHistoryTapped {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Clear History"
+                                                                   message:@"Are you sure you want to delete all history?"
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Delete" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        [HistoryManager clearHistory];
+        self.historyItems = @[];
+        [self.tableView reloadData];
+//        NSLog(@"History wiped"); //
+    }];
+    
+    [alert addAction:cancel];
+    [alert addAction:confirm];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 #pragma mark - Table view data source
