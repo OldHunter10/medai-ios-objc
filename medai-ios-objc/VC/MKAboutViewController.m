582acbc0139b311b11a57210f7930bd21f0b6cf7
JC <g1447236976@gmail.com>
2025-08-07 01:04:58 +0800

    make about page scrollable with stack layout

--- medai-ios-objc/VC/MKAboutViewController.m
+++ medai-ios-objc/VC/MKAboutViewController.m
@@ -19,56 +19,60 @@
     self.title = @"About";
     self.view.backgroundColor = [UIColor whiteColor];
     
+    UIScrollView *scrollView = [[UIScrollView alloc] init];
+    scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:scrollView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [scrollView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor],
+        [scrollView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+        [scrollView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+        [scrollView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+    ]];
+    
+    UIStackView *stackView = [[UIStackView alloc] init];
+    stackView.axis = UILayoutConstraintAxisVertical;
+    stackView.spacing = 20;
+    stackView.alignment = UIStackViewAlignmentCenter;
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [scrollView addSubview:stackView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [stackView.topAnchor constraintEqualToAnchor:scrollView.topAnchor constant:40],
+        [stackView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:30],
+        [stackView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-30],
+        [stackView.bottomAnchor constraintEqualToAnchor:scrollView.bottomAnchor constant:-40],
+        [stackView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor constant:-60],
+    ]];
+    
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.text = @"MedAI";
     titleLabel.font = [UIFont boldSystemFontOfSize:28];
-    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
     UILabel *versionLabel = [[UILabel alloc] init];
     versionLabel.text = [NSString stringWithFormat:@"Version %@", [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]];
     versionLabel.textColor = [UIColor grayColor];
-    versionLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
     UILabel *descLabel = [[UILabel alloc] init];
     descLabel.text = @"AI-assisted symptom exploration tool.";
-    descLabel.numberOfLines = 0;
     descLabel.textAlignment = NSTextAlignmentCenter;
-    descLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    descLabel.numberOfLines = 0;
     
     UILabel *authorLabel = [[UILabel alloc] init];
     authorLabel.text = @"Developed by JC";
     authorLabel.font = [UIFont italicSystemFontOfSize:14];
     authorLabel.textColor = [UIColor darkGrayColor];
-    authorLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
     UIButton *githubButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [githubButton setTitle:@"View on GitHub" forState:UIControlStateNormal];
     [githubButton addTarget:self action:@selector(openGitHub) forControlEvents:UIControlEventTouchUpInside];
-    githubButton.translatesAutoresizingMaskIntoConstraints = NO;
     
-    [self.view addSubview:titleLabel];
-    [self.view addSubview:versionLabel];
-    [self.view addSubview:descLabel];
-    [self.view addSubview:authorLabel];
-    [self.view addSubview:githubButton];
-    
-    [NSLayoutConstraint activateConstraints:@[
-        [titleLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [titleLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:40],
-        
-        [versionLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [versionLabel.topAnchor constraintEqualToAnchor:titleLabel.bottomAnchor constant:8],
-        
-        [descLabel.topAnchor constraintEqualToAnchor:versionLabel.bottomAnchor constant:20],
-        [descLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [descLabel.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.7],
-        
-        [authorLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [authorLabel.topAnchor constraintEqualToAnchor:descLabel.bottomAnchor constant:30],
-        
-        [githubButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-        [githubButton.topAnchor constraintEqualToAnchor:authorLabel.bottomAnchor constant:20]
-    ]];
+    [stackView addArrangedSubview:titleLabel];
+    [stackView addArrangedSubview:versionLabel];
+    [stackView addArrangedSubview:descLabel];
+    [stackView addArrangedSubview:authorLabel];
+    [stackView addArrangedSubview:githubButton];
 }
 
 - (void)openGitHub {
