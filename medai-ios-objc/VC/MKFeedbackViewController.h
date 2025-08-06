9a4055aab259f7c1fc87afceb62d5fb7a18a4d92
JC <g1447236976@gmail.com>
2025-08-07 07:43:00 +0800

    add Feedback page with input and submit

---
+++ medai-ios-objc/VC/MKFeedbackViewController.h
@@ -0,0 +1,16 @@
+//
+//  MKFeedbackViewController.h
+//  medai-ios-objc
+//
+//  Created by steph on 2025/8/7.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MKFeedbackViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ medai-ios-objc/VC/MKFeedbackViewController.m
@@ -0,0 +1,31 @@
+//
+//  MKFeedbackViewController.m
+//  medai-ios-objc
+//
+//  Created by steph on 2025/8/7.
+//
+
+#import "MKFeedbackViewController.h"
+
+@interface MKFeedbackViewController ()
+
+@end
+
+@implementation MKFeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
